8d4ea864977dc00d5f88573019b2ec50cdf14d86
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2026-07-30 14:05:47 +0200

    config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,15 @@ import Aura from "@primeuix/themes/aura";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   css: ["@/assets/css/main.css"],
+
   vite: {
     plugins: [tailwindcss()],
   },
-  nitro: {
-    static: true,
-  },
+
   modules: ["@primevue/nuxt-module", "@nuxt/image"],
+
   primevue: {
     autoImport: true,
 
