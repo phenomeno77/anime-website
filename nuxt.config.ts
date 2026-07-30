4fa70d047dd4a3152a62e1bf31849a862036b236
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2026-07-30 21:01:02 +0200

    11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  nitro: {
+    static: true,
+  },
   modules: ["@primevue/nuxt-module", "@nuxt/image"],
   primevue: {
     autoImport: true,
