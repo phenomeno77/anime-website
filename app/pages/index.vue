dbc14569460460f047c7ee63e620bcd85abb7940
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2026-08-01 21:27:42 +0200

    qq

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,19 +5,31 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="parallax">
+  <div
+    class="parallax"
+    style="background-image: url(&quot;/background-home/hero.jpg&quot;)"
+  >
     <HeroSelection class="px-4 md:px-16" />
   </div>
 
-  <div class="parallax">
+  <div
+    class="parallax"
+    style="background-image: url(&quot;/background-home/features.jpg&quot;)"
+  >
     <FeaturesSelection class="px-4 md:px-16" />
   </div>
 
-  <div class="parallax">
+  <div
+    class="parallax"
+    style="background-image: url(&quot;/background-home/gallery.jpg&quot;)"
+  >
     <Gallery class="px-4 md:px-16" />
   </div>
 
-  <div class="parallax">
+  <div
+    class="parallax"
+    style="background-image: url(&quot;/background-home/hero.jpg&quot;)"
+  >
     <Commision class="px-4 md:px-16" />
   </div>
 </template>
