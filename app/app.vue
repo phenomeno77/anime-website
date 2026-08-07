634215527d31770081e143cfa07c3ba959989d7b
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2026-08-07 23:33:31 +0200

    SEO update

--- app/app.vue
+++ app/app.vue
@@ -6,8 +6,13 @@ useSeoMeta({
   ogTitle: "Anime & Digital Art Commissions",
   ogDescription:
     "Custom hand-painted anime PC glass panels and digital artwork crafted to order.",
-  ogImage: "/background-pages/1.webp",
+  // ogImage: "https://yourdomain.com/background-pages/1.webp",
+  ogImage: "public/artwork-gallery/artworks-home/003.webp",
+
+  ogImageWidth: 1200,
+  ogImageHeight: 630,
   twitterCard: "summary_large_image",
+  twitterImage: "public/artwork-gallery/artworks-home/003.webp",
 });
 </script>
 
